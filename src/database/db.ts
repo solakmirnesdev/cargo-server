1166ddd0529b62bf35eca3bf4e8f2c62faf78126
Mirnes Solak <solakmirnes.dev@gmail.com>
2023-09-04 22:08:04 +0200

    Updating env

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 export async function connectDatabase (): Promise<void> {
   try {
-    await mongoose.connect('mongodb://localhost:27017/cargodb')
-    console.log('SUCCESS: Connected to MongoDB')
+    await mongoose.connect(`mongodb://localhost:27017/${process.env.MONGO_DATABASE}`)
+    console.log(`SUCCESS: Connected to MongoDB - Local database name: ${process.env.MONGO_DATABASE}`)
   } catch (error) {
     console.error('ERROR: Connecting to MongoDB:', error)
     throw error
