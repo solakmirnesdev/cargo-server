4a7dd17ca6188cdd9a8961aa60bba24a50aee9ec
Mirnes Solak <solakmirnes.dev@gmail.com>
2023-09-04 22:42:47 +0200

    Modifyng tsconfig

--- dist/config/routeConfig.js
+++ dist/config/routeConfig.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const UserController_1 = require("@controllers/UserController");
+const UserController_1 = require("../controllers/UserController");
 const configureRoutes = (app) => {
     // Define user routes
     app.post('/users', UserController_1.createUser);
